8202405d4395bcd4e39bc1d273430c2212061054
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-06-28 23:22:56 +1200

    3hr 3m adding animation to walkers

--- code/moving1.gd
+++ code/moving1.gd
@@ -14,3 +14,14 @@ func _physics_process(delta: float) -> void:
 	var current_velocity = (global_position - last_position) / delta
 	last_position = global_position
 	velocity = current_velocity
+	$side.global_rotation = 0
+	$forward.global_rotation = 0
+	$CollisionShape2D.global_rotation = 0
+	if cos(2*PI*($".".global_rotation_degrees)/180) < 0:
+		$side.play("no")
+		$forward.play("them")
+		$CollisionShape2D.scale.y = 1
+	else:
+		$forward.play("no")
+		$side.play("them")
+		$CollisionShape2D.scale.y = 0.3

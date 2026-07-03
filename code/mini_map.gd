c413bb48667c47b355d23298db9d43d11ce9584e
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-07-03 18:03:41 +1200

    8hr 35m adding frame around player

--- code/mini_map.gd
+++ code/mini_map.gd
@@ -10,5 +10,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$Node2D.position.x = (Global.character_position.x)/34.08
 	$Node2D.position.y = (Global.character_position.y)/34.08
-	if Global.clas == 'pe':
-		$Node2D2/Sprite2D.global_position = $Node2D2/pe.global_position
+	print(Global.clas)
+	if Global.clas == 'PE':
+		$Node2D2/pointer.global_position = $Node2D2/pe.global_position
+	if Global.clas == 'ENG':
+		$Node2D2/pointer.global_position = $Node2D2/eng.global_position
+	if Global.clas == 'MAT':
+		$Node2D2/pointer.global_position = $Node2D2/mat.global_position
+	if Global.clas == 'RE':
+		$Node2D2/pointer.global_position = $Node2D2/re.global_position
+	if Global.clas == 'CSC':
+		$Node2D2/pointer.global_position = $Node2D2/csc.global_position
+	if Global.clas == 'PHY':
+		$Node2D2/pointer.global_position = $Node2D2/phy.global_position
+	if Global.clas == 'CHE':
+		$Node2D2/pointer.global_position = $Node2D2/che.global_position
+	if Global.clas == 'BIO':
+		$Node2D2/pointer.global_position = $Node2D2/bio.global_position
+	if Global.clas == 'SST':
+		$Node2D2/pointer.global_position = $Node2D2/sst.global_position
